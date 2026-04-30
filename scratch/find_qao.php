cafe39c38b7cd3b008e4a2806a3930b39410ffc5
Tedeyy <tedjus666@gmail.com>
2026-04-30 14:30:33 +0800

    update gitignore and remove scratch files from tracking

--- scratch/find_qao.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/database.php';
-$db = (new Database())->getConnection();
-$stmt = $db->query("SELECT office_id, name FROM divisions_offices WHERE name LIKE '%Quality Assurance%'");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
